cd88912bb926caceb005345ea4d45c08bfea8cd4
Alex Regiani <alexregianideveloper@outlook.com>
2023-05-17 15:40:06 -0400

    the card has it's own file now

--- lib/features/products/presentation/pages/products_page.dart
+++ lib/features/products/presentation/pages/products_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../blocs/product_request/list_product_bloc.dart';
+import '../widgets/product_card.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({Key? key}) : super(key: key);
@@ -17,27 +18,7 @@ class ProductsPage extends StatelessWidget {
             },
             child: Text('press to get products'),
           ),
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            elevation: 10,
-            shadowColor: Colors.grey.withOpacity(0.4),
-            clipBehavior: Clip.antiAlias,
-            color: const Color(0xFFC8FAFF),
-            child: Container(
-              width: 250,
-              child: Column(children: [
-                Image.asset(fit: BoxFit.cover, 'assets/chair.jpg'),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                  child: Column(
-                    children: [Text('Item name'), Text('Price')],
-                  ),
-                ),
-              ]),
-            ),
-          ),
+          ProductCard(),
         ]),
       ),
     );
--- lib/features/products/presentation/widgets/product_card.dart
+++ lib/features/products/presentation/widgets/product_card.dart
@@ -1,10 +1,30 @@
-// import 'package:flutter/material.dart';
-//
-// class ProductCard extends StatelessWidget {
-//   const ProductCard({Key? key}) : super(key: key);
-//
-//   // @override
-//   // Widget build(BuildContext context) {
-//   //   return
-//   // }
-// }
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      elevation: 10,
+      shadowColor: Colors.grey.withOpacity(0.4),
+      clipBehavior: Clip.antiAlias,
+      color: const Color(0xFFC8FAFF),
+      child: Container(
+        width: 250,
+        child: Column(children: [
+          Image.asset(fit: BoxFit.cover, 'assets/chair.jpg'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            child: Column(
+              children: [Text('Item name'), Text('Price')],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
