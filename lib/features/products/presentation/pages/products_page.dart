8b07c68d35508e2349ac48595ee84e8a8eac0570
Alex Regiani <alexregianideveloper@outlook.com>
2023-05-19 15:59:38 -0400

    more cosmetic changes

--- lib/features/products/presentation/pages/products_page.dart
+++ lib/features/products/presentation/pages/products_page.dart
@@ -10,47 +10,43 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Flexible(
-            flex: 1,
-            child: TextButton(
-              onPressed: () {
-                BlocProvider.of<ListProductBloc>(context).add(const FetchProductEvent());
-              },
-              child: Text('press to get products'),
-            ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        TextButton(
+          onPressed: () {
+            BlocProvider.of<ListProductBloc>(context).add(const FetchProductEvent());
+          },
+          child: Text('press to get products'),
+        ),
+        Center(
+          child: BlocBuilder<ListProductBloc, ListProductState>(
+            builder: (context, state) {
+              if (state is ListProductLoadingState) {
+                return const CircularProgressIndicator();
+              } else if (state is ListProductSuccessState) {
+                return SizedBox(
+                  height: 420,
+                  child: ListView.builder(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: state.products.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ProductCard(
+                            product: state.products[index],
+                          ),
+                        );
+                      }),
+                );
+              } else if (state is ListProductErrorState) {
+                return Text(state.errorMessage);
+              } else {
+                return Container();
+              }
+            },
           ),
-          Center(
-            child: BlocBuilder<ListProductBloc, ListProductState>(
-              builder: (context, state) {
-                if (state is ListProductLoadingState) {
-                  return const CircularProgressIndicator();
-                } else if (state is ListProductSuccessState) {
-                  return Expanded(
-                    child: SizedBox(
-                      height: 420,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: state.products.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                              child: ProductCard(product: state.products[index]),
-                            );
-                          }),
-                    ),
-                  );
-                } else if (state is ListProductErrorState) {
-                  return Text(state.errorMessage);
-                } else {
-                  return Container();
-                }
-              },
-            ),
-          ),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 }
--- lib/features/products/presentation/widgets/product_card.dart
+++ lib/features/products/presentation/widgets/product_card.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:store_flutter/features/products/domain/entities/product_response_entity.dart';
 
 class ProductCard extends StatelessWidget {
-  ProductCard({Key? key, required this.product}) : super(key: key);
+  ProductCard({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
   final ProductResponseEntity product;
 
   @override
@@ -19,47 +22,56 @@ class ProductCard extends StatelessWidget {
         clipBehavior: Clip.antiAlias,
         color: const Color(0xFFFFFFFF),
         child: SizedBox(
-          width: 250,
+          width: 250, // controls width of the card (in general)
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
                 child: Container(
                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-                  width: 300,
-                  height: 300,
+                  width: 200, // controls width of the image
+                  height: 250, // controls height of the image
                   child: Image.network(
-                    product.image,
+                    product.image, // product image
                   ),
                 ),
               ),
               Expanded(
+                //this expanded is in charge putting the title on top and the price and icon on bottom of the card
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                  child: Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                            product.title),
-                        Align(
-                            alignment: Alignment.bottomLeft,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(style: const TextStyle(fontSize: 16), '\$${product.price.toString()}'),
-                                  Icon(Icons.add_circle, size: 30),
-                                ],
-                              ),
-                            )),
-                      ],
-                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Divider(
+                        height: 0,
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                      Expanded(
+                        // it puts title further on the top, closer to the image
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical:
+                                  8), // the expanded by itself puts the title too close to the image, this padding fixes that
+                          child: Text(
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                              product.title),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0), // price and icon padding
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(style: const TextStyle(fontSize: 15), '\$${product.price.toString()}'),
+                            const Icon(Icons.add_circle, size: 40),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
